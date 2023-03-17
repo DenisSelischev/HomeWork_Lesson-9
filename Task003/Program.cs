8f131a06de5c5d62423c7403866952231752d6c9
DenisSelischev <Deni-SelischeV23@yandex.ru>
2023-03-17 22:07:23 +0300

    Solved 3 task

---
+++ Task003/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите начальное число M:");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите начальное число N:");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int AckermanFunction (int numberM, int numberN)
+{
+    if (numberM == 0) return numberN + 1;
+    if (numberM != 0 && numberN == 0) return AckermanFunction(numberM - 1, 1);
+    if (numberM > 0 && numberN > 0) return AckermanFunction(numberM - 1, AckermanFunction(numberM, numberN - 1));
+return AckermanFunction(numberM, numberN);
+}
+
+Console.WriteLine($"Функция Аккермана для чисел A({numberM},{numberN}) = {AckermanFunction(numberM, numberN)}");
